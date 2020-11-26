bb384a58b2d14a289b94eb08235b8ac3c3f2d269
Adrian Bielawski <adrian@bielaw.ski>
2020-11-26 11:00:34 +0000

    Initialize project using Create React App

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from 'global-styles/app.scss'
+
+const App = () => {
+  return (
+    <div className={styles.app}>
+    </div>
+  )
+}
+
+export default App
---
+++ webpack.common.js
@@ -0,0 +1,63 @@
+const path = require('path');
+
+module.exports = {
+    entry: {
+        app: './src/index.tsx',
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    resolve: {
+        alias: {
+            components: path.join(__dirname, 'src', 'components'),
+            store: path.join(__dirname, 'src', 'store'),
+            global_styles: path.join(__dirname, 'src', 'global_styles'),
+            assets: path.join(__dirname, 'assets'),
+        },
+        modules: [
+            path.join(__dirname, 'src'),
+            path.join(__dirname, 'assets'),
+            'node_modules'
+        ],
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    module: {
+        rules: [
+            {
+              test: /\.(ts|tsx)$/,
+              loader: "ts-loader",
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: "html-loader"
+                    }
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: {
+                                localIdentName: '[name]_[local]_[hash:base64:3]',
+                                exportLocalsConvention: 'dashes',
+                            },
+                        },
+                    },
+                    'sass-loader',
+                ],
+            },
+            {
+                test: /\.(png|jpg)$/,
+                use: [
+                    'file-loader',
+                ]
+            }
+        ]
+    },
+};
