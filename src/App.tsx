0c3e5b133eb736f8a05215ec6c3a21c2491fd632
Adrian Bielawski <adrian@bielaw.ski>
2020-11-29 10:11:51 +0000

    Min height and resize listener added to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 import styles from 'global-styles/app.scss'
+//Redux
+import { useTypedSelector } from 'store/reducers/index'
+import { changeScreenHeight } from 'store/actions/appActions'
+//Custom components
 import Header from 'components/global-components/header/header'
 
 const App = () => {
+  const dispatch = useDispatch()
+  const screenHeight = useTypedSelector(state => state.app.screenHeight)
+
+  const handleResize = () => {
+      dispatch(changeScreenHeight(window.innerHeight))
+  }
+
+  useEffect(() => {
+      window.addEventListener('resize', handleResize)
+      return () => {
+          window.removeEventListener('resize', handleResize)
+      }
+  }, [])
   return (
+    <div className={styles.app} style={{minHeight: screenHeight}}>
       <Header />
     </div>
   )
