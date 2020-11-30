8c0831f6c0f4be91e7858518391e15f63bb31d8b
Adrian Bielawski <adrian@bielaw.ski>
2020-11-30 12:35:11 +0000

    Login component added to main screen

--- src/components/main/main.tsx
+++ src/components/main/main.tsx
@@ -1,15 +1,24 @@
 import React from 'react'
+import { useLocation } from 'react-router-dom'
 import styles from './main.scss'
 //Redux
 //Custom components
 import Header from 'components/global-components/header/header'
+import Login from './login/login'
 
 const Main = () => {
-  return (
-    <div className={styles.main}>
-        <Header />
-    </div>
-  )
+    const location = useLocation()
+    return (
+        <div className={styles.main}>
+            <Header />
+            <div className={styles.content}>
+                {location.pathname === '/signup' ?
+                    <p>Signup</p> :
+                    <Login />
+                }
+            </div>
+        </div>
+    )
 }
 
 export default Main
