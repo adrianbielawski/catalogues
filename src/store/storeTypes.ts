245edc19902fdbdf1060c66c6eb820c41860efe0
Adrian Bielawski <adrian@bielaw.ski>
2020-11-30 12:24:27 +0000

    appThunk refactored

--- src/store/storeTypes.ts
+++ src/store/storeTypes.ts
@@ -1,4 +1,14 @@
+import { ThunkAction as BaseThunkAction } from 'redux-thunk'
+import { Action } from 'redux'
+import { RootState } from 'store/reducers/index'
 export const SCREEN_HEIGHT_CHANGED = 'APP/SCREEN_HEIGHT_CHANGED'
+
+export type ThunkAction<ReturnType = void> = BaseThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>
 
 export interface AppState {
     screenHeight: number,
