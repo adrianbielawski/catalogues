e9639f7bcbe7e811d0e6565f0c0680a9176dc5b0
Adrian Bielawski <adrian@bielaw.ski>
2020-11-30 11:23:01 +0000

    UserDeserializer added

---
+++ src/globalTypes.ts
@@ -0,0 +1,17 @@
+export interface User {
+    id: number,
+    username: string,
+    email: string,
+    image: string,
+    image_thumbnail: string,
+    is_anonymous: boolean,
+}
+
+export interface DeserializedUser {
+    id: number,
+    username: string,
+    email: string,
+    image: string,
+    imageThumbnail: string,
+    isAnonymous: boolean,
+}
---
+++ src/serializers.ts
@@ -0,0 +1,10 @@
+import { User, DeserializedUser } from 'src/globalTypes'
+
+export const userDeserializer = (user: User): DeserializedUser => ({
+    id: user.id,
+    username: user.username,
+    email: user.email,
+    image: user.image,
+    imageThumbnail: user.image_thumbnail,
+    isAnonymous: user.is_anonymous,
+})
