c47d5419030cea1e1840bc00079128b5037b22af
Adrian Bielawski <adrian@bielaw.ski>
2020-12-14 18:47:27 +0000

    ImagesCarousel added to EditItemModal

--- src/components/main/catalogues/catalogue/catalogue-item/edit-item-modal/editItemModal.tsx
+++ src/components/main/catalogues/catalogue/catalogue-item/edit-item-modal/editItemModal.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import styles from './editItemModal.scss'
 //Types
 import { DeserializedItem } from 'src/globalTypes'
 //Custom components
 import Modal from 'components/global-components/modal/modal'
 import EditableList from 'components/global-components/editable-list/editableList'
+import ImagesCarousel from 'components/global-components/images-carousel/imagesCarousel'
 
 type Props = {
     show: boolean,
@@ -14,7 +15,27 @@ type Props = {
 type OnConfirm = (input: string[]) => void
 
 const EditItemModal = (props: Props) => {
+    const editItemRef = useRef<HTMLDivElement>(null)
+    const [width, setWidth] = useState(0)
     const modalParent = document.getElementById('catalogueMainContent')
+
+    useEffect(() => {
+        window.addEventListener('resize', handleResize)
+        if (editItemRef.current !== null && props.show) {
+            handleResize()
+        } else {
+            setWidth(0)
+        }
+        return () => {
+            window.removeEventListener('resize', handleResize)
+        }
+    }, [props.show])
+
+    const handleResize = () => {
+        if (editItemRef.current !== null) {
+            setWidth(editItemRef.current!.getBoundingClientRect().width)
+        }
+    }
 
     const handleClose = () => {
         props.onClose()
@@ -35,9 +56,45 @@ const EditItemModal = (props: Props) => {
         },
     ]
 
+    const IMAGES = [
+        {
+            url: 'http://placekitten.com/300/300',
+            isMain: false,
+        },
+        {
+            url: 'http://placekitten.com/300/300',
+            isMain: true,
+        },
+        {
+            url: 'http://placekitten.com/300/300',
+            isMain: false,
+        },
+        {
+            url: 'http://placekitten.com/300/400',
+            isMain: false,
+        },
+        {
+            url: 'http://placekitten.com/120/90',
+            isMain: false,
+        },
+        {
+            url: 'http://placekitten.com/200/120',
+            isMain: false,
+        },
+        {
+            url: 'http://placekitten.com/200/240',
+            isMain: false,
+        },
+        {
+            url: 'http://placekitten.com/200/300',
+            isMain: false,
+        },
+    ]
+
     return (
         <Modal show={props.show} parent={modalParent!} onClose={handleClose}>
-            <div className={styles.editItemModal}>
+            <div className={styles.editItemModal} ref={editItemRef} >
+                <ImagesCarousel width={width * .7} images={IMAGES} />
                 <EditableList className={styles.editableList} fields={FIELDS} />
             </div>
         </Modal>
