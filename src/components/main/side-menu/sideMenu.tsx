19197fd79b6e9b7ce7aa2bb07c1de8e72400289d
Adrian Bielawski <adrian@bielaw.ski>
2020-12-17 09:24:45 +0000

    SideMenu in settings refactored to use SideBar component

--- src/components/main/side-menu/sideMenu.tsx
+++ src/components/main/side-menu/sideMenu.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import styles from './sideMenu.scss'
 //Custom components
 import SideMenuItem from './side-menu-item/sideMenuItem'
+import SideBar from 'components/global-components/side-bar/sideBar'
 
 type ItemType = {
     title: string,
@@ -13,26 +14,23 @@ interface Props {
     content: ItemType[],
 }
 
-type GetItems = () => React.ReactNode
-
 const SideMenu = (props: Props) => {
-
-    const getItems: GetItems = () => {
-        return props.content.map((item, index) => {
-                return (
-                    <SideMenuItem
-                        title={item.title}
-                        url={item.url}
-                        key={index}
-                    />
-                )
-        })
+    const getItems = (): React.ReactNode => {
+        return props.content.map((item, index) => (
+            <SideMenuItem
+                title={item.title}
+                url={item.url}
+                key={index}
+            />
+        ))
     }
 
     return (
-        <ul className={styles.sideMenu}>
-            {getItems()}
-        </ul>
+        <SideBar className={styles.sideMenu}>
+            <ul>
+                {getItems()}
+            </ul>
+        </SideBar>
     )
 }
 
