c13ad0b99a151a7f67c40919520a191215398c3f
Adrian Bielawski <adrian@bielaw.ski>
2020-12-19 14:56:28 +0000

    Button component refactored

--- src/components/global-components/button/button.tsx
+++ src/components/global-components/button/button.tsx
@@ -4,24 +4,18 @@ import classNames from 'classnames/bind'
 
 interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {
     className?: string,
-    round?: boolean,
 }
 
 const cx = classNames.bind(styles)
 
 const Button = (props: Props) => {
-    const {round, ...rest} = props
-
     const buttonClass = cx(
         'button',
         props.className,
-        {
-            round,
-        }
     )
 
     return (
-        <button {...rest} className={buttonClass}>
+        <button {...props} className={buttonClass}>
             {props.children}
         </button>
     )
