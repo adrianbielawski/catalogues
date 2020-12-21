b45ec8ab100336c5fecfa5d03a02814eac96fcf9
Adrian Bielawski <adrian@bielaw.ski>
2020-12-21 17:15:31 +0000

    FiltersBar adjusted for mobile devices

--- src/components/main/catalogues/catalogue/filters-bar/filtersBar.tsx
+++ src/components/main/catalogues/catalogue/filters-bar/filtersBar.tsx
@@ -1,21 +1,47 @@
 import React from 'react'
+import classNames from 'classnames/bind'
 import styles from './filtersBar.scss'
+//Context
+import useFiltersBarContext from '../filters-bar/useFiltersBarContext'
 //Custom components
 import SideBar from 'components/global-components/side-bar/sideBar'
 import Filters from './filters/filters'
 import Sort from './sort/sort'
 import Search from './search/search'
+import FiltersBarButton from './filters-bar-button/filtersBarButton'
+
+const cx = classNames.bind(styles)
 
 const FiltersBar = () => {
+    const filtersBarContext = useFiltersBarContext()
+    const screenWidth = window.innerWidth
+
+    const toggleFiltersBar = () => {
+        filtersBarContext.filtersBar.toggleFiltersBar()
+    }
+
+    const filtersBarWrapperClass = cx(
+        'filtersBarWrapper',
+        {
+            active: filtersBarContext.filtersBar.show,
+        }
+    )
     return (
-        <SideBar className={styles.filtersBar}>
-            <>
-                <Search />
-                <Sort />
-                <Filters />
-            </>
-        </SideBar>
+        <div className={filtersBarWrapperClass}>
+            <SideBar className={styles.filtersBar}>
+                <>
+                    <Search />
+                    <Sort />
+                    <Filters />
+                </>
+            </SideBar>
+            {screenWidth <= 640 &&
+                <div className={styles.background} onClick={toggleFiltersBar}></div>
+            }
+        </div>
     )
 }
+
+FiltersBar.FiltersBarButton = FiltersBarButton
 
 export default FiltersBar
