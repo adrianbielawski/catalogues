39b3f542a656c03944672c6c6b583ac756580035
Adrian Bielawski <adrian@bielaw.ski>
2021-01-12 09:19:33 +0000

    Loaders added to ManageCatalogues
    Show loaders when adding new catalogue and fetching catalogues

--- src/components/main/settings/account-settings/manage-catalogues/manageCatalogues.tsx
+++ src/components/main/settings/account-settings/manage-catalogues/manageCatalogues.tsx
@@ -7,14 +7,19 @@ import { createCatalogue, getCatalogues } from 'store/actions/cataloguesActions'
 //Custom components
 import AddButton from 'components/global-components/add-button/addButton'
 import ManageCatalogue from './manage-catalogue/manageCatalogue'
+import Loader from 'components/global-components/loader/loader'
 
 const ManageCatalogues = () => {
     const dispatch = useDispatch()
     const user = useTypedSelector(state => state.app.user)
     const catalogues = useTypedSelector(state => state.catalogues.catalogues)
+    const fetchingCatalogues = useTypedSelector(state => state.catalogues.fetchingCatalogues)
+    const creatingNewCatalogue = useTypedSelector(state => state.catalogues.creatingNewCatalogue)
 
     useEffect(() => {
-        dispatch(getCatalogues(user!.id))
+        if (catalogues.length === 0) {
+            dispatch(getCatalogues(user!.id))
+        }
     }, [])
 
     const handleAddCatalogueClick = () => {
@@ -29,9 +34,13 @@ const ManageCatalogues = () => {
         <div className={styles.manageCatalogues}>
             <AddButton
                 text="Add new catalogue"
+                loading={creatingNewCatalogue}
                 className={styles.addButton}
                 onClick={handleAddCatalogueClick}
             />
+            {fetchingCatalogues &&
+                <Loader size={50} className={styles.loader} />
+            }
             {items}
         </div>
     )
