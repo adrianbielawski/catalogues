b76f8a1a419c0bb17f19effbb3e7672c92908108
Adrian Bielawski <adrian@bielaw.ski>
2021-01-26 10:23:42 +0000

    Dispatch addItemToState added to catalogue component

--- src/components/main/catalogues/catalogue/catalogue.tsx
+++ src/components/main/catalogues/catalogue/catalogue.tsx
@@ -1,16 +1,16 @@
 import React, { useEffect } from 'react'
 import { RouteComponentProps } from 'react-router-dom'
-import styles from './catalogue.scss'
 import { useDispatch } from 'react-redux'
+import styles from './catalogue.scss'
 //Redux
-import { fetchCatalogueFields } from 'store/actions/cataloguesActions'
+import { addItemToState, fetchCatalogueFields } from 'store/actions/cataloguesActions'
+import { useTypedSelector } from 'store/reducers'
+import { catalogueSelectorBySlug } from 'store/selectors'
 //Custom components
 import CatalogueItems from './catalogue-items/catalogueItems'
 import FiltersBar from './filters-bar/filtersBar'
 import AddButton from 'components/global-components/add-button/addButton'
 import FixedAddButton from 'components/global-components/fixed-add-button/FixedAddButton'
-import { useTypedSelector } from 'store/reducers'
-import { catalogueSelectorBySlug } from 'store/selectors'
 
 type Params = {
     slug: string
@@ -26,7 +26,7 @@ const Catalogue = (props: RouteComponentProps<Params>) => {
     }, [])
 
     const handleAddItem = () => {
-
+        dispatch(addItemToState(catalogue.id))
     }
 
     return (
