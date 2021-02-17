c5a14690af60e9b378edeb17f8e4d366d77fad53
Adrian Bielawski <adrian@bielaw.ski>
2021-02-17 14:39:09 +0000

    isElementInViewport function removed, use intersectionObserver in CatalogueItems instead

--- src/components/main/catalogues/catalogue/catalogue-items/catalogueItems.tsx
+++ src/components/main/catalogues/catalogue/catalogue-items/catalogueItems.tsx
@@ -1,12 +1,11 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useCallback, useEffect, useRef, useState } from 'react'
 import classNames from 'classnames/bind'
 import styles from './catalogueItems.scss'
 //Redux
 import { FETCH_ITEMS } from 'store/slices/cataloguesSlices/itemsDataSlice.ts/itemsDataSlice'
 import { useAppDispatch, useTypedSelector } from 'store/storeConfig'
-//Custo hooks and utils
+//Custom hooks
 import { useDelay } from 'src/customHooks'
-import { isElementInViewport } from 'src/utils'
 //Custom components
 import Loader from 'components/global-components/loader/loader'
 import CatalogueItem from '../catalogue-item/catalogueItem'
@@ -20,8 +19,9 @@ const cx = classNames.bind(styles)
 
 const CatalogueItems = (props: Props) => {
     const dispatch = useAppDispatch()
-    const itemRef = useRef<HTMLLIElement>(null)
-    const [lastItemInView, setLastItemInView] = useState(false)
+    const [lastItem, setLastItem] = useState<HTMLLIElement | null>()
+    const lastItemRef = useCallback(setLastItem, [])
+    const observer = useRef<IntersectionObserver | null>()
     const itemsData = useTypedSelector(state => state.itemsData)
     const delayCompleted = useDelay(itemsData.fetchingItems)
 
@@ -30,22 +30,33 @@ const CatalogueItems = (props: Props) => {
     }, [props.catalogueId])
 
     useEffect(() => {
-        if (lastItemInView && itemsData.next && itemsData.next > 2) {
+        if (lastItem) {
+            createObserver(lastItem)
+        }
+        return () => {
+            if (observer.current) {
+                observer.current.disconnect()
+            }
+        }
+    }, [lastItem])
+
+    const handleIntersect = (entries: IntersectionObserverEntry[]) => {
+        if (entries[0].isIntersecting && itemsData.next && itemsData.next > 2) {
             fetchItems()
         }
-    }, [lastItemInView])
+    }
 
-    useEffect(() => {
-        window.addEventListener('scroll', isLastItemInViewport)
-
-        return () => {
-            window.removeEventListener('scroll', isLastItemInViewport)
+    const createObserver = (element: Element) => {
+        if (observer.current) {
+            observer.current.disconnect()
         }
-    }, [])
+        observer.current = new IntersectionObserver(handleIntersect)
+        observer.current.observe(element)
+    }
 
     const fetchItems = () => {
         let page = 1
-        
+
         if (itemsData.catalogueId === props.catalogueId) {
             page = itemsData.next || 1
         }
@@ -55,14 +66,8 @@ const CatalogueItems = (props: Props) => {
         }))
     }
 
-    const isLastItemInViewport = () => {
-        if (itemRef.current) {
-            setLastItemInView(isElementInViewport(itemRef.current))
-        }
-    }
-
     const getItems = () => itemsData.results.map((item, i) => {
-        let ref = i === itemsData.results.length - 1 ? itemRef : null
+        const ref = i === itemsData.results.length - 1 ? lastItemRef : null
         return <CatalogueItem item={item} key={item.id} ref={ref} />
     })
 
--- src/utils.ts
+++ src/utils.ts
@@ -1,16 +1,5 @@
 import { LegacyRef, MutableRefObject, RefCallback } from "react"
 
-export const isElementInViewport = (el: HTMLElement) => {
-    var rect = el.getBoundingClientRect()
-
-    return (
-        rect.bottom > 0 &&
-        rect.right > 0 &&
-        rect.left < (window.innerWidth || document.documentElement.clientWidth) &&
-        rect.top < (window.innerHeight || document.documentElement.clientHeight)
-    )
-}
-
 export const scrollTop = () => {
     document.body.scrollTo({ top: 0, behavior: 'smooth' })
     document.documentElement.scrollTo({ top: 0, behavior: 'smooth' })
