b48907c0a5253bdae3aee4cd7f0bdb5821be8a59
Adrian Bielawski <adrian@bielaw.ski>
2021-02-18 15:50:28 +0000

    Catalogue name change validation added

--- src/components/main/settings/account-settings/manage-catalogues/manage-catalogue/catalogue-title/catalogueTitle.tsx
+++ src/components/main/settings/account-settings/manage-catalogues/manage-catalogue/catalogue-title/catalogueTitle.tsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './catalogueTitle.scss'
 //Redux
-import { CHANGE_CATALOGUE_NAME, TOGGLE_CATALOGUE_NAME_EDIT } from 'store/slices/cataloguesSlices/cataloguesSlice/cataloguesSlice'
+import {
+    CHANGE_CATALOGUE_NAME, TOGGLE_CATALOGUE_NAME_EDIT
+} from 'store/slices/cataloguesSlices/cataloguesSlice/cataloguesSlice'
 import { useAppDispatch, useTypedSelector } from 'store/storeConfig'
 import { catalogueSelector } from 'store/selectors'
 //Custom components
 import EditableField from 'components/global-components/editable-field/editableField'
+import MessageModal from 'components/global-components/message-modal/messageModal'
 
 type Props = {
     id: number,
@@ -14,17 +17,38 @@ type Props = {
 
 const CatalogueTitle = (props: Props) => {
     const dispatch = useAppDispatch()
-    const catalogue = useTypedSelector(catalogueSelector(props.id))  
+    const [error, setError] = useState('')
+    const catalogue = useTypedSelector(catalogueSelector(props.id))
 
-    const handleEditName = () => {
-        dispatch(TOGGLE_CATALOGUE_NAME_EDIT(props.id))
+    const validateName = (name: string) => {
+        let error = null
+        if(!name.length) {
+            error = `Please set catalogue name`
+        }
+        return {
+            valid: error === null,
+            error,
+        }
     }
 
     const handleNameChange = (input: string[]) => {
+        const { valid, error } = validateName(input[0])
+        if (!valid) {
+            setError(error!)
+            return
+        }
         dispatch(CHANGE_CATALOGUE_NAME({
             catalogueId: props.id,
             name: input[0]
         }))
+    }
+    
+    const clearError = () => {
+        setError('')
+    }
+
+    const handleEditName = () => {
+        dispatch(TOGGLE_CATALOGUE_NAME_EDIT(props.id))
     }
 
     return (
@@ -37,6 +61,12 @@ const CatalogueTitle = (props: Props) => {
                 isSubmitting={catalogue.isSubmittingCatalogueName}
                 onEditClick={handleEditName}
                 onConfirm={handleNameChange}
+            />
+            <MessageModal
+                show={error.length !== 0}
+                title={'Catalogue name error'}
+                message={error}
+                onConfirm={clearError}
             />
         </div>
     )
