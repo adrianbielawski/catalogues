5a587f4fdd79fc021a8d1d62b80f46512485cafc
Adrian Bielawski <adrian@bielaw.ski>
2021-02-19 10:09:20 +0000

    Reducers extracted from itemsDataSlice

---
+++ src/store/slices/cataloguesSlices/itemsDataSlice.ts/itemDataReducers.ts
@@ -0,0 +1,143 @@
+import { PayloadAction } from '@reduxjs/toolkit'
+import { mod } from 'src/utils'
+import { itemDeserializer, listDeserializer } from 'src/serializers'
+import { DeserializedItem } from 'src/globalTypes'
+import * as T from './itemsDataTypes'
+import { getFieldsValuesById, getFieldValueById, getItemById } from './ItemsDataSelectors'
+
+type State = T.ItemsDataState
+
+export const fetchItemReducers = {
+    REFRESH_ITEM(state: State, action: PayloadAction<T.FetchItemPayload>) {},
+    FETCH_ITEM(state: State, action: PayloadAction<T.FetchItemPayload>) {},
+    FETCH_ITEM_START(state: State, action: PayloadAction<number>) {},
+    FETCH_ITEM_SUCCESS(state: State, action: PayloadAction<T.FetchItemSuccessPayload>) {
+        const item = getItemById(state, action.payload.itemId)
+        Object.assign(item, itemDeserializer(action.payload.data))
+    },
+    FETCH_ITEM_FAILURE(state: State, action: PayloadAction<number | string>) {
+        const item = getItemById(state, action.payload)
+        item.isSubmitting = false
+    },
+}
+
+export const fetchItemsReducers = {
+    FETCH_ITEMS(state: State, action: PayloadAction<T.FetchItemsPayload>) {},
+    FETCH_ITEMS_START(state: State) {
+        state.fetchingItems = false
+    },
+    FETCH_ITEMS_SUCCESS(state: State, action: PayloadAction<T.FetchItemsSuccessPayload>) {
+        const list = listDeserializer(action.payload.data, itemDeserializer)
+        if (state.catalogueId !== action.payload.catalogueId) {
+            list.startIndex = 1
+        }
+        return {
+            ...list,
+            results: state.results.concat(list.results),
+            catalogueId: action.payload.catalogueId,
+            fetchingItems: false,
+        }
+    },
+    FETCH_ITEMS_FAILURE(state: State) {
+        state.fetchingItems = false
+    },
+}
+
+export const EditItemReducers = {
+    TOGGLE_EDIT_ITEM(state: State, action: PayloadAction<number | string>) {
+        const item = getItemById(state, action.payload)
+        item.isEditing = !item.isEditing
+    },
+    ADD_ITEM_TO_STATE(state: State, action: PayloadAction<number>) {
+        const item = {
+            id: `newItem_${Date.now()}`,
+            createdBy: null,
+            createdAt: '',
+            modifiedAt: '',
+            catalogueId: action.payload,
+            fieldsValues: [],
+            images: [],
+            removedImages: [],
+            isEditing: true,
+            isSubmitting: false,
+        }
+
+        state.results.unshift(item)
+    },
+    REMOVE_ITEM_FROM_STATE(state: State, action: PayloadAction<number | string>) {
+        state.results = state.results.filter(i => i.id !== action.payload)
+    },
+    CHANGE_ITEM_FIELD_VALUE(state: State, action: PayloadAction<T.ItemAndFieldIdPayload>) {
+        const fieldsValues = getFieldsValuesById(state, action.payload.itemId)
+        const fieldValue = fieldsValues.filter(f => f.fieldId === action.payload.fieldId)[0]
+
+        const newField = {
+            itemId: action.payload.itemId,
+            fieldId: action.payload.fieldId,
+            value: action.payload.value,
+        }
+
+        if (fieldValue) {
+            const selectedFieldValue = getFieldValueById(
+                state,
+                action.payload.itemId,
+                action.payload.fieldId,
+            )
+            Object.assign(selectedFieldValue, newField)
+        } else {
+            fieldsValues.push(newField)
+        }
+    },
+}
+
+export const itemImageReducers = {
+    ADD_IMAGE_TO_STATE(state: State, action: PayloadAction<T.AddImageToStatePayload>) {
+        const item = getItemById(state, action.payload.itemId)
+
+        const image = {
+            id: `newImage${Date.now()}`,
+            image: action.payload.image,
+            imageThumbnail: '',
+            isPrimary: item.images.length === 0,
+            itemId: item.id,
+        }
+        item.images.push(image)
+    },
+    REMOVE_IMAGE_FROM_STATE(state: State, action: PayloadAction<T.ImagePayload>) {
+        const item = getItemById(state, action.payload.itemId)
+        const removedImage = item.images[action.payload.index]
+
+        if (removedImage.isPrimary) {
+            let newPrimaryIndex = mod(action.payload.index + 1, item.images.length)
+            item.images[newPrimaryIndex].isPrimary = true
+        }
+
+        if (!removedImage.id.toString().startsWith('newImage')) {
+            item.removedImages.push(removedImage)
+        }
+        item.images.splice(action.payload.index, 1)
+    },
+    CHANGE_PRIMARY_IMAGE(state: State, action: PayloadAction<T.ImagePayload>) {
+        const item = getItemById(state, action.payload.itemId)
+        const prevPrimary = item.images.findIndex(img => img.isPrimary)
+        item.images[prevPrimary].isPrimary = false
+        item.images[action.payload.index].isPrimary = true
+    },
+}
+
+export const saveItem = {
+    SAVE_ITEM(state: State, action: PayloadAction<DeserializedItem>) {},
+    SAVE_ITEM_START(state: State, action: PayloadAction<number | string>) {
+        const item = getItemById(state, action.payload)
+        item.isSubmitting = true
+    },
+    SAVE_ITEM_SUCCESS(state: State, action: PayloadAction<T.SaveItemSuccessPayload>) {
+        const item = getItemById(state, action.payload.prevId)
+        item.isSubmitting = false
+        item.isEditing = false
+    },
+    SAVE_ITEM_FAILURE(state: State, action: PayloadAction<number | string>) {
+        const item = getItemById(state, action.payload)
+        item.isSubmitting = false
+    },
+}
--- src/store/slices/cataloguesSlices/itemsDataSlice.ts/itemsDataSlice.ts
+++ src/store/slices/cataloguesSlices/itemsDataSlice.ts/itemsDataSlice.ts
@@ -1,11 +1,7 @@
-import { createAction, createSlice, PayloadAction } from '@reduxjs/toolkit'
-import { cloneDeep } from 'lodash'
-import { mod } from 'src/utils'
+import { createSlice } from '@reduxjs/toolkit'
 import { CLEAR_APP_STATE } from 'store/slices/appSlices/appSlice'
-import { itemDeserializer, listDeserializer } from 'src/serializers'
-import { DeserializedItem } from 'src/globalTypes'
 import * as T from './itemsDataTypes'
-import { getFieldsValuesById, getFieldValueById, getItemById } from './ItemsDataSelectors'
+import * as itemReducers from './itemDataReducers'
 
 const initialState: T.ItemsDataState = {
     catalogueId: null,
@@ -20,133 +16,15 @@ const initialState: T.ItemsDataState = {
     results: [],
 }
 
-export const REFRESH_ITEM = createAction<T.FetchItemPayload>('ITEMS_DATA/REFRESH_ITEM')
-export const FETCH_ITEM = createAction<T.FetchItemPayload>('ITEMS_DATA/FETCH_ITEM')
-export const FETCH_ITEM_START = createAction<number>('ITEMS_DATA/FETCH_ITEM_START')
-export const FETCH_ITEMS = createAction<T.FetchItemsPayload>('ITEMS_DATA/FETCH_ITEMS')
-export const SAVE_ITEM = createAction<DeserializedItem>('ITEMS_DATA/SAVE_ITEM')
-
 export const itemsDataSlice = createSlice({
     name: 'ITEMS_DATA',
     initialState,
     reducers: {
-        FETCH_ITEM_SUCCESS(state, action: PayloadAction<T.FetchItemSuccessPayload>) {
-            const item = getItemById(state, action.payload.itemId)
-            Object.assign(item, itemDeserializer(action.payload.data))
-        },
-        FETCH_ITEM_FAILURE(state, action: PayloadAction<number | string>) {
-            const item = getItemById(state, action.payload)
-            item.isSubmitting = false
-        },
-        FETCH_ITEMS_START(state) {
-            state.fetchingItems = false
-        },
-        FETCH_ITEMS_SUCCESS(state, action: PayloadAction<T.FetchItemsSuccessPayload>) {
-            const prevState = cloneDeep(state)
-            const list = listDeserializer(action.payload.data, itemDeserializer)
-            if (prevState.catalogueId === action.payload.catalogueId) {
-                list.startIndex = prevState.startIndex || 1
-                list.results.unshift(...prevState.results)
-            }
-            const newState: T.ItemsDataState = {
-                ...list,
-                catalogueId: action.payload.catalogueId,
-                fetchingItems: false,
-            }
-            return newState
-        },
-        FETCH_ITEMS_FAILURE(state) {
-            state.fetchingItems = false
-        },
-        TOGGLE_EDIT_ITEM(state, action: PayloadAction<number | string>) {
-            const item = getItemById(state, action.payload)
-            item.isEditing = !item.isEditing
-        },
-        ADD_ITEM_TO_STATE(state, action: PayloadAction<number>) {
-            const item = {
-                id: `newItem_${Date.now()}`,
-                createdBy: null,
-                createdAt: '',
-                modifiedAt: '',
-                catalogueId: action.payload,
-                fieldsValues: [],
-                images: [],
-                removedImages: [],
-                isEditing: true,
-                isSubmitting: false,
-            }
-
-            state.results.unshift(item)
-        },
-        REMOVE_ITEM_FROM_STATE(state, action: PayloadAction<number | string>) {
-            state.results = state.results.filter(i => i.id !== action.payload)
-        },
-        CHANGE_ITEM_FIELD_VALUE(state, action: PayloadAction<T.ItemAndFieldIdPayload>) {
-            const fieldsValues = getFieldsValuesById(state, action.payload.itemId)
-            const fieldValue = fieldsValues.filter(f => f.fieldId === action.payload.fieldId)[0]
-
-            const newField = {
-                itemId: action.payload.itemId,
-                fieldId: action.payload.fieldId,
-                value: action.payload.value,
-            }
-
-            if (fieldValue) {
-                const selectedFieldValue = getFieldValueById(
-                    state,
-                    action.payload.itemId,
-                    action.payload.fieldId,
-                )
-                Object.assign(selectedFieldValue, newField)
-            } else {
-                fieldsValues.push(newField)
-            }
-        },
-        ADD_IMAGE_TO_STATE(state, action: PayloadAction<T.AddImageToStatePayload>) {
-            const item = getItemById(state, action.payload.itemId)
-
-            const image = {
-                id: `newImage${Date.now()}`,
-                image: action.payload.image,
-                imageThumbnail: '',
-                isPrimary: item.images.length === 0,
-                itemId: item.id,
-            }
-            item.images.push(image)
-        },
-        REMOVE_IMAGE_FROM_STATE(state, action: PayloadAction<T.ImagePayload>) {
-            const item = getItemById(state, action.payload.itemId)
-            const removedImage = item.images[action.payload.index]
-
-            if (removedImage.isPrimary) {
-                let newPrimaryIndex = mod(action.payload.index + 1, item.images.length)
-                item.images[newPrimaryIndex].isPrimary = true
-            }
-
-            if (!removedImage.id.toString().startsWith('newImage')) {
-                item.removedImages.push(removedImage)
-            }
-            item.images.splice(action.payload.index, 1)
-        },
-        CHANGE_PRIMARY_IMAGE(state, action: PayloadAction<T.ImagePayload>) {
-            const item = getItemById(state, action.payload.itemId)
-            const prevPrimary = item.images.findIndex(img => img.isPrimary)
-            item.images[prevPrimary].isPrimary = false
-            item.images[action.payload.index].isPrimary = true
-        },
-        SAVE_ITEM_START(state, action: PayloadAction<number | string>) {
-            const item = getItemById(state, action.payload)
-            item.isSubmitting = true
-        },
-        SAVE_ITEM_SUCCESS(state, action: PayloadAction<T.SaveItemSuccessPayload>) {
-            const item = getItemById(state, action.payload.prevId)
-            item.isSubmitting = false
-            item.isEditing = false
-        },
-        SAVE_ITEM_FAILURE(state, action: PayloadAction<number | string>) {
-            const item = getItemById(state, action.payload)
-            item.isSubmitting = false
-        },
+        ...itemReducers.fetchItemReducers,
+        ...itemReducers.fetchItemsReducers,
+        ...itemReducers.EditItemReducers,
+        ...itemReducers.itemImageReducers,
+        ...itemReducers.saveItem,
         CLEAR_ITEMS_DATA() {
             return initialState
         },
@@ -157,8 +35,8 @@ export const itemsDataSlice = createSlice({
 })
 
 export const {
-    FETCH_ITEM_SUCCESS, FETCH_ITEM_FAILURE,
-    FETCH_ITEMS_START, FETCH_ITEMS_SUCCESS, FETCH_ITEMS_FAILURE,
+    REFRESH_ITEM, FETCH_ITEM, FETCH_ITEM_SUCCESS, FETCH_ITEM_START, FETCH_ITEM_FAILURE,
+    FETCH_ITEMS, FETCH_ITEMS_START, FETCH_ITEMS_SUCCESS, FETCH_ITEMS_FAILURE,
     TOGGLE_EDIT_ITEM,
     ADD_ITEM_TO_STATE,
     REMOVE_ITEM_FROM_STATE,
@@ -166,6 +44,6 @@ export const {
     ADD_IMAGE_TO_STATE,
     REMOVE_IMAGE_FROM_STATE,
     CHANGE_PRIMARY_IMAGE,
-    SAVE_ITEM_START, SAVE_ITEM_SUCCESS, SAVE_ITEM_FAILURE,
+    SAVE_ITEM, SAVE_ITEM_START, SAVE_ITEM_SUCCESS, SAVE_ITEM_FAILURE,
     CLEAR_ITEMS_DATA,
 } = itemsDataSlice.actions
