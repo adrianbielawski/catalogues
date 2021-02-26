68140adac9738891bc043efc4caa278435e1c538
Adrian Bielawski <adrian@bielaw.ski>
2021-02-26 15:59:55 +0000

    Modal styles tefactored

--- src/components/global-components/modal/modal.tsx
+++ src/components/global-components/modal/modal.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import classNames from 'classnames/bind';
 import styles from './modal.scss';
@@ -6,7 +6,6 @@ import styles from './modal.scss';
 type Props = {
     show: boolean,
     children: JSX.Element,
-    parent?: HTMLElement,
     className?: string,
     onClose?: () => void
 }
@@ -14,43 +13,10 @@ type Props = {
 const cx = classNames.bind(styles)
 
 const Modal = (props: Props) => {
-    const modalHeight = props.parent?.getBoundingClientRect().height
-    const modalWidth = props.parent?.getBoundingClientRect().width
-    const [top, setTop] = useState(0)
-    const [isMounted, setIsMounted] = useState(false)
-
-    const updatePosition = () => {
-        setTop(window.pageYOffset)
-    }
-
-    useEffect(() => {
-        if (props.show === true) {
-            setIsMounted(true)
-        }
-    }, [props.show])
-
-    useEffect(() => {
-        window.addEventListener('scroll', updatePosition)
-
-        return () => {
-            window.removeEventListener('scroll', updatePosition)
-        }
-    }, [])
-
     const modalClass = cx(
         'modal',
         props.className,
-        {
-            mounted: isMounted,
-            elevated: !props.parent,
-        },
     )
-
-    const modalStyles = {
-        top: props.parent ? 0 : top,
-        minHeight: props.parent ? `${modalHeight}px` : '100%',
-        width: props.parent ? `${modalWidth}px` : '100%',
-    }
 
     const handleWrapperClick = (e: React.MouseEvent) => {
         e.stopPropagation()
@@ -58,18 +24,17 @@ const Modal = (props: Props) => {
 
     const handleClose = () => {
         if (props.onClose) {
-            setIsMounted(false)
             props.onClose()
         }
     }
 
     return (
         props.show ? ReactDOM.createPortal(
-            <div className={modalClass} style={modalStyles} onClick={handleClose}>
+            <div className={modalClass} onClick={handleClose}>
                 <div onClick={handleWrapperClick}>
                     {props.children}
                 </div>
-            </div>, props.parent || document.body
+            </div>, document.body
         ) : null
     )
 }
