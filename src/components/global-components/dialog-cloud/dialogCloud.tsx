d1be4ceef51bceb85d832036e52aa0a829ce9be9
Adrian Bielawski <adrian@bielaw.ski>
2021-03-02 10:24:53 +0000

    DialogCloud component created and added to Input component

---
+++ src/components/global-components/dialog-cloud/dialogCloud.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import classNames from 'classnames/bind'
+import styles from './dialogCloud.scss'
+
+type Props = {
+    message: string,
+}
+
+const cx = classNames.bind(styles)
+
+const DialogCloud = (props: Props) => {
+
+    const dialogCloudClass = cx(
+        'dialogCloud',
+        {
+            active: props.message.length > 0,
+        }
+    )
+
+    return (
+        <div className={dialogCloudClass}>
+            <p>{props.message}</p>
+        </div>
+    )
+}
+
+export default DialogCloud
--- src/components/global-components/input/input.tsx
+++ src/components/global-components/input/input.tsx
@@ -3,9 +3,11 @@ import styles from './input.scss'
 import classNames from 'classnames/bind'
 //Custom hooks
 import { mergeRefs } from 'src/utils'
+import DialogCloud from '../dialog-cloud/dialogCloud'
 
 interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
     className?: string,
+    invalidInputMessage?: string,
 }
 const cx = classNames.bind(styles)
 
@@ -13,20 +15,23 @@ const Input: React.ForwardRefRenderFunction<
     HTMLInputElement,
     Props
 > = (props, ref) => {
-    const { className, ...rest } = props
+    const { className, invalidInputMessage, ...rest } = props
     const inputRef = useRef<HTMLInputElement>(null)
 
-    const inputClass = cx(
-        'input',
+    const wrapperClass = cx(
+        'wrapper',
         className,
     )
+    
     return (
-        <input
-            className={inputClass}
-            {...rest}
-            ref={mergeRefs([ref, inputRef])}
-            spellCheck="false"
-        />
+        <div className={wrapperClass}>
+            <DialogCloud message={invalidInputMessage || ''} />
+            <input
+                {...rest}
+                ref={mergeRefs([ref, inputRef])}
+                spellCheck="false"
+            />
+        </div>
     )
 }
 
