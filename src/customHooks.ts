109acff1ba7e631c4f25ff8b734efdb9cffeff66
Adrian Bielawski <adrian@bielaw.ski>
2021-03-03 14:49:13 +0000

    useDebouncedCallback hook created

--- src/customHooks.ts
+++ src/customHooks.ts
@@ -73,3 +73,33 @@ export const useDebouncedDispatch = (
 
     return inputRef
 }
+
+export const useDebouncedCallback = (
+    callback: (value: string) => void,
+    debounceDuration: number = 500,
+    validator: (value: string) => boolean = () => true,
+) => {
+    const [input, setInput] = useState<HTMLInputElement | null>()
+    const inputRef = useCallback(setInput, [])
+
+    useEffect(() => {
+        if (!input) {
+            return
+        }
+
+        const events$ = fromEvent<React.ChangeEvent<HTMLInputElement>>(input, 'input')
+
+        const sub = events$.pipe(
+            map(e => e.target!.value),
+            filter(validator),
+            debounceTime(debounceDuration),
+            distinctUntilChanged(),
+        ).subscribe(callback)
+
+        return () => {
+            sub.unsubscribe()
+        }
+    }, [input])
+
+    return inputRef
+}
