a98d51702a19a5102d5e87a925db88853b1d0373
Adrian Bielawski <adrian@bielaw.ski>
2021-03-04 13:01:32 +0000

    Default catalogue option added to catalogue settings

--- src/components/main/settings/account-settings/manage-catalogues/manage-catalogue/catalogue-settings/catalogueSettings.tsx
+++ src/components/main/settings/account-settings/manage-catalogues/manage-catalogue/catalogue-settings/catalogueSettings.tsx
@@ -2,14 +2,25 @@ import React from 'react'
 import { faCogs } from '@fortawesome/free-solid-svg-icons'
 //Types
 import { DeserializedCatalogue } from 'src/globalTypes'
+//Redux
+import { useAppDispatch } from 'store/storeConfig'
+import { CHANGE_DEFAULT_CATALOGUE } from 'store/slices/cataloguesSlices/cataloguesSlice/cataloguesSlice'
 //Custom components
 import CatalogueSettingsCategory from '../catalogueSettingsCategory/catalogueSettingsCategory'
+import CheckBoxWithTitle from 'components/global-components/check-box-with-title/checkBoxWithTitle'
 
 type Props = {
     catalogue: DeserializedCatalogue,
 }
 
 const CatalogueSettings = (props: Props) => {
+    const dispatch = useAppDispatch()
+    const handleChangeDefault = () => {
+        dispatch(CHANGE_DEFAULT_CATALOGUE({
+            catalogueId: props.catalogue.id,
+            default: !props.catalogue.default,
+        }))
+    }
 
     return (
         <CatalogueSettingsCategory
@@ -17,6 +28,12 @@ const CatalogueSettings = (props: Props) => {
             icon={faCogs}
         >
             <>
+                <CheckBoxWithTitle
+                    id={'defaultCatalogue'}
+                    title={`Default catalogue`}
+                    selected={props.catalogue.default}
+                    onChange={handleChangeDefault}
+                />
             </>
         </CatalogueSettingsCategory>
     )
