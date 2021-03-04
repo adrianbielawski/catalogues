86a93efddf07242d7088c6093b11671d6e5e1bd9
Adrian Bielawski <adrian@bielaw.ski>
2021-03-04 15:41:30 +0000

    ManageCatalogue component refactored

--- src/components/main/settings/account-settings/manage-catalogues/manage-catalogue/manageCatalogue.tsx
+++ src/components/main/settings/account-settings/manage-catalogues/manage-catalogue/manageCatalogue.tsx
@@ -2,9 +2,6 @@ import React from 'react'
 import styles from './manageCatalogue.scss'
 //Types
 import { DeserializedCatalogue } from 'src/globalTypes'
-//Redux
-import { useTypedSelector } from 'store/storeConfig'
-import { catalogueSelector } from 'store/selectors'
 //Custom components
 import CatalogueTitle from './catalogue-title/catalogueTitle'
 import CatalogueFields from './catalogue-fields/catalogueFields'
@@ -14,21 +11,17 @@ type Props = {
     catalogue: DeserializedCatalogue,
 }
 
-const ManageCatalogue = (props: Props) => {
-    const catalogue = useTypedSelector(catalogueSelector(props.catalogue.id))
-
-    return (
-        <div className={styles.manageCatalogue}>
-            <CatalogueTitle
-                id={props.catalogue.id}
-                name={props.catalogue.name}
-            />
-            <div className={styles.content}>
-                <CatalogueSettings catalogue={catalogue} />
-                <CatalogueFields catalogue={catalogue} />
-            </div>
+const ManageCatalogue = (props: Props) => (
+    <div className={styles.manageCatalogue}>
+        <CatalogueTitle
+            id={props.catalogue.id}
+            name={props.catalogue.name}
+        />
+        <div className={styles.content}>
+            <CatalogueSettings catalogue={props.catalogue} />
+            <CatalogueFields catalogue={props.catalogue} />
         </div>
-    )
-}
+    </div>
+)
 
 export default ManageCatalogue
