e861748b4812663a8987c10900e5ac9321901246
Adrian Bielawski <adrian@bielaw.ski>
2021-03-04 14:28:58 +0000

    ProtectedConfirmMessageModal component created

---
+++ src/components/global-components/protected-confirm-message-modal/protectedConfirmMessageModal.tsx
@@ -0,0 +1,73 @@
+import React, { useState } from 'react'
+import styles from './protectedConfirmMessageModal.scss'
+//Custom Components
+import Modal from 'components/global-components/modals/modal'
+import Button from '../button/button'
+import Input from 'components/global-components/input/input'
+
+export interface ProtectedMessage {
+    title?: string,
+    value: string,
+    expectedInput: string,
+}
+
+type Props = {
+    show: boolean,
+    message: ProtectedMessage | null,
+    onConfirm: () => void,
+    onCancel: () => void,
+}
+
+const ProtectedConfirmMessageModal = (props: Props) => {
+    const [inputVal, setInputVal] = useState('')
+
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setInputVal(e.target.value)
+    }
+
+    if (props.message === null) {
+        return null
+    }
+
+    const disabled = inputVal !== props.message.expectedInput
+
+    return (
+        <Modal
+            show={props.show}
+            className={styles.modal}
+        >
+            <div className={styles.confirmMessageModal}>
+                {props.message.title &&
+                    <p className={styles.title}>{props.message.title}</p>
+                }
+                <p className={styles.message}>{props.message.value}</p>
+                <p className={styles.inputMessage}>
+                    If so, please tap
+                    <span>{` "${props.message.expectedInput}" `}</span>
+                    below
+                </p>
+                <Input
+                    autoFocus
+                    onChange={handleInputChange}
+                />
+                <div className={styles.buttons}>
+                    <Button
+                        className={styles.button}
+                        disabled={disabled}
+                        onClick={props.onConfirm}
+                    >
+                        Ok
+                </Button>
+                    <Button
+                        className={styles.button}
+                        onClick={props.onCancel}
+                    >
+                        Cancel
+                </Button>
+                </div>
+            </div>
+        </Modal>
+    )
+}
+
+export default ProtectedConfirmMessageModal
