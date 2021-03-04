bf62a639d11f83ca783dccf4506a1898cd7a1f61
Adrian Bielawski <adrian@bielaw.ski>
2021-03-04 10:06:11 +0000

    CatalogueSettingsCategory component created

---
+++ src/components/main/settings/account-settings/manage-catalogues/manage-catalogue/catalogueSettingsCategory/catalogueSettingsCategory.tsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react'
+import classNames from 'classnames/bind'
+import styles from './catalogueSettingsCategory.scss'
+//Types
+import { IconDefinition } from '@fortawesome/fontawesome-svg-core'
+//Redux
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+type Props = {
+    title: string,
+    icon: IconDefinition,
+    children: JSX.Element | HTMLElement,
+}
+
+const cx = classNames.bind(styles)
+
+const CatalogueSettingsCategory = (props: Props) => {
+    const [active, setActive] = useState(false)
+
+    const toggleShowContent = () => {
+        setActive(!active)
+    }
+
+    const contentClass = cx(
+        'content',
+        { active },
+    )
+    const titleClass = cx(
+        'title',
+        { active },
+    )
+
+    return (
+        <div className={styles.catalogueSettingsCategory}>
+            <div
+                className={titleClass}
+                onClick={toggleShowContent}
+            >
+                <FontAwesomeIcon
+                    className={styles.icon}
+                    icon={props.icon}
+                />
+                <p>{props.title}</p>
+            </div>
+            <div className={contentClass}>
+                {props.children}
+            </div>
+        </div>
+    )
+}
+
+export default CatalogueSettingsCategory
