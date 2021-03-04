e860dbc51361dabe3cf7e0f769a298672fbe83a5
Adrian Bielawski <adrian@bielaw.ski>
2021-03-04 14:33:00 +0000

    Delete catalogue button and ProtectedConfirmMessageModal added to catalogue settings

--- src/components/main/settings/account-settings/manage-catalogues/manage-catalogue/catalogue-settings/catalogueSettings.tsx
+++ src/components/main/settings/account-settings/manage-catalogues/manage-catalogue/catalogue-settings/catalogueSettings.tsx
@@ -1,13 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { faCogs } from '@fortawesome/free-solid-svg-icons'
+import styles from './catalogueSettings.scss'
 //Types
 import { DeserializedCatalogue } from 'src/globalTypes'
 //Redux
 import { useAppDispatch } from 'store/storeConfig'
-import { CHANGE_DEFAULT_CATALOGUE } from 'store/slices/cataloguesSlices/cataloguesSlice/cataloguesSlice'
+import { CHANGE_DEFAULT_CATALOGUE, DELETE_CATALOGUE } from 'store/slices/cataloguesSlices/cataloguesSlice/cataloguesSlice'
 //Custom components
 import CatalogueSettingsCategory from '../catalogueSettingsCategory/catalogueSettingsCategory'
 import CheckBoxWithTitle from 'components/global-components/check-box-with-title/checkBoxWithTitle'
+import Button from 'components/global-components/button/button'
+import
+    ProtectedConfirmMessageModal, { ProtectedMessage }
+from 'components/global-components/protected-confirm-message-modal/protectedConfirmMessageModal'
 
 type Props = {
     catalogue: DeserializedCatalogue,
@@ -15,6 +20,8 @@ type Props = {
 
 const CatalogueSettings = (props: Props) => {
     const dispatch = useAppDispatch()
+    const [message, setMessage] = useState<ProtectedMessage | null>(null)
+
     const handleChangeDefault = () => {
         dispatch(CHANGE_DEFAULT_CATALOGUE({
             catalogueId: props.catalogue.id,
@@ -22,20 +29,52 @@ const CatalogueSettings = (props: Props) => {
         }))
     }
 
+    const handleDeleteCatalogue = () => {
+        setMessage({
+            title: 'Delete catalogue',
+            value: `Are you sure you want to delete "${props.catalogue.name}" catalogue`,
+            expectedInput: props.catalogue.name,
+        })
+    }
+
+    const deleteCatalogue = () => {
+        setMessage(null)
+        dispatch(DELETE_CATALOGUE(props.catalogue.id))
+    }
+
+    const clearMessage = () => {
+        setMessage(null)
+    }
+
     return (
-        <CatalogueSettingsCategory
-            title={'Catalogue settings'}
-            icon={faCogs}
-        >
-            <>
-                <CheckBoxWithTitle
-                    id={'defaultCatalogue'}
-                    title={`Default catalogue`}
-                    selected={props.catalogue.default}
-                    onChange={handleChangeDefault}
-                />
-            </>
-        </CatalogueSettingsCategory>
+        <div className={styles.catalogueSettings}>
+            <CatalogueSettingsCategory
+                title={'Catalogue settings'}
+                icon={faCogs}
+            >
+                <>
+                    <CheckBoxWithTitle
+                        id={'defaultCatalogue'}
+                        title={`Default catalogue`}
+                        selected={props.catalogue.default}
+                        onChange={handleChangeDefault}
+                    />
+                    <Button
+                        className={styles.deleteCatalogueButton}
+                        disabled={props.catalogue.deletingCatalogue}
+                        onClick={handleDeleteCatalogue}
+                    >
+                        Delete catalogue
+                    </Button>
+                </>
+            </CatalogueSettingsCategory>
+            <ProtectedConfirmMessageModal
+                show={message !== null}
+                message={message}
+                onConfirm={deleteCatalogue}
+                onCancel={clearMessage}
+            />
+        </div>
     )
 }
 
