1fa877e149afb510775a89a2f6a050bba94ac9c9
Adrian Bielawski <adrian@bielaw.ski>
2021-03-08 16:32:47 +0000

    ClassName added to CheckBoxWithTitle props

--- src/components/global-components/check-box-with-title/checkBoxWithTitle.tsx
+++ src/components/global-components/check-box-with-title/checkBoxWithTitle.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import classNames from 'classnames/bind'
 import styles from './checkBoxWithTitle.scss'
 //Custom components
 import CheckBox from 'components/global-components/check-box/checkBox'
@@ -11,13 +12,20 @@ type Props = {
     onChange: (id: number | string, selected: boolean) => void
 }
 
+const cx = classNames.bind(styles)
+
 const CheckBoxWithTitle = (props: Props) => {
     const handleCheck = () => {
         props.onChange(props.id, !props.selected)
     }
 
+    const checkBoxWithTitleClass = cx(
+        'checkBoxWithTitle',
+        props.className,
+    )
+
     return (
-        <div className={styles.checkBoxWithTitle} onClick={handleCheck}>
+        <div className={checkBoxWithTitleClass} onClick={handleCheck}>
             <CheckBox selected={props.selected} />
             <span>{props.title}</span>
         </div>
