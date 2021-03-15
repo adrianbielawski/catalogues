0bf35ce070d78b1a6875ec39d49fe38f6af0e3d5
Adrian Bielawski <adrian@bielaw.ski>
2021-03-15 14:09:51 +0000

    UserRating component created and added to ItemRating component

--- src/components/main/catalogues/catalogue/catalogue-item/item-rating/itemRating.tsx
+++ src/components/main/catalogues/catalogue/catalogue-item/item-rating/itemRating.tsx
@@ -2,19 +2,37 @@ import React from 'react'
 import styles from './itemRating.scss'
 //Types
 import { DeserializedItem } from 'src/globalTypes'
+//Redux
+import { useAppDispatch } from 'store/storeConfig'
+import { CHANGE_ITEM_RATING } from 'store/slices/cataloguesSlices/itemsDataSlice.ts/itemsDataSlice'
 //Custom components
 import AverageRating from 'components/global-components/average-rating/averageRating'
+import UserRating from 'components/global-components/user-rating/userRating'
 
 type Props = {
     item: DeserializedItem,
 }
 
 const ItemRating = (props: Props) => {
+    const dispatch = useAppDispatch()
+
+    const handleRatingChange = (rating: number) => {
+        dispatch(CHANGE_ITEM_RATING({
+            itemId: props.item.id,
+            rating,
+        }))
+    }
+
     return (
         <div className={styles.itemRating}>
             <AverageRating
                 rating={props.item.rating.average}
                 count={props.item.rating.count}
+            />
+            <UserRating
+                rating={props.item.rating.currentUser}
+                range={5}
+                onChange={handleRatingChange}
             />
         </div>
     )
