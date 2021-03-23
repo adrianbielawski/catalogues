dc95d8dd70c2698f90fdf3a6df4509971a11b9c1
Adrian Bielawski <adrian@bielaw.ski>
2021-03-23 13:57:04 +0000

    CatalogueHeader component with nav and catalogue name created

---
+++ src/components/main/catalogues/catalogue/catalogue-header/catalogueHeader.tsx
@@ -0,0 +1,78 @@
+import React, { useContext } from 'react'
+import { faFolderOpen } from '@fortawesome/free-solid-svg-icons'
+import classNames from 'classnames/bind'
+import styles from './catalogueHeader.scss'
+//Types
+import { DeserializedCatalogue } from 'src/globalTypes'
+//Context
+import { NavContext } from 'components/global-components/nav/nav-store/navStore'
+import NavContextProvider from 'components/global-components/nav/nav-store/navContextProvider'
+//Redux
+import { useTypedSelector } from 'store/storeConfig'
+//custom components
+import UserImage from 'components/global-components/user-image/userImage'
+import Nav from 'components/global-components/nav/nav'
+
+const contextValue = {
+    show: false,
+    listId: null,
+}
+
+type Props = {
+    catalogue: DeserializedCatalogue,
+    className?: string,
+}
+
+const cx = classNames.bind(styles)
+
+const CatalogueHeader = (props: Props) => {
+    const { show } = useContext(NavContext)
+    const currentUser = useTypedSelector(state => state.currentUser)
+    const catalogues = useTypedSelector(state => state.catalogues)
+
+    const NAV_ITEMS = [
+        {
+            id: 'User',
+            title: currentUser.user!.username,
+            icon: (
+                <UserImage
+                    className={styles.userImage}
+                    url={currentUser.user?.imageThumbnail}
+                />
+            ),
+            url: `/${currentUser.user!.username}`
+        },
+        {
+            id: 'Catalogues',
+            title: `${currentUser.user?.username}'s catalogues`,
+            faIcon: faFolderOpen,
+            children: catalogues.catalogues.map(c => ({
+                id: c.name,
+                title: c.name,
+                faIcon: faFolderOpen,
+                url: `/${currentUser.user!.username}/catalogues/${c.slug}`,
+            })),
+        },
+    ]
+
+    const headerClass = cx(
+        'header',
+        props.className,
+    )
+
+    return (
+        <NavContextProvider value={contextValue}>
+            <div className={headerClass}>
+                <Nav show={show} items={NAV_ITEMS} />
+                <p className={styles.catalogueName}>
+                    {props.catalogue.name}
+                </p>
+                <div className={styles.social}>
+
+                </div>
+            </div>
+        </NavContextProvider>
+    )
+}
+
+export default CatalogueHeader
