f8e3cff095c2cc0a55e5e527184a4bf0a024dd10
Adrian Bielawski <adrian@bielaw.ski>
2021-03-23 10:13:26 +0000

    AuthUser catalogues added to catalogues state, created fetch catalogues epic and reducers

--- src/components/main/main.tsx
+++ src/components/main/main.tsx
@@ -7,6 +7,7 @@ import { HydratedRouteComponentProps, PrivateRouteWithContext, RouteWithContext 
 //Redux
 import { useTypedSelector } from 'store/storeConfig'
 import { CLEAR_CURRENT_USER_MESSAGE, GET_CURRENT_USER } from 'store/slices/currentUserSlices/currentUserSlice'
+import { FETCH_AUTH_USER_CATALOGUES } from 'store/slices/cataloguesSlices/cataloguesSlice/cataloguesSlice'
 //Custom components
 import DeprecatedHeader from 'components/global-components/deprecated-header/header'
 import Catalogues from './catalogues/catalogues'
@@ -28,6 +29,13 @@ const Main = (props: HydratedRouteComponentProps) => {
         }
         dispatch(GET_CURRENT_USER(username))
     }, [username])
+
+    useEffect(() => {
+        if (!user?.id) {
+            return
+        }
+        dispatch(FETCH_AUTH_USER_CATALOGUES())
+    }, [user])
 
     const handleCloseMessage = () => {
         dispatch(CLEAR_CURRENT_USER_MESSAGE())
--- src/store/slices/cataloguesSlices/cataloguesSlice/cataloguesSlice.ts
+++ src/store/slices/cataloguesSlices/cataloguesSlice/cataloguesSlice.ts
@@ -5,6 +5,10 @@ import * as cataloguesReducers from './reducers/cataloguesReducer'
 import * as fieldsReducers from './reducers/fieldsReducer'
 
 const initialState: CataloguesState = {
+    authUser: {
+        catalogues: [],
+        fetchingCatalogues: true,
+    },
     catalogues: [],
     fetchingCatalogues: true,
     creatingNewCatalogue: false,
@@ -18,6 +22,7 @@ export const cataloguesSlice = createSlice({
         ...cataloguesReducers.createCatalogueReducers,
         ...cataloguesReducers.refreshCatalogueReducers,
         ...cataloguesReducers.fetchCataloguesReducers,
+        ...cataloguesReducers.fetchAuthUserCataloguesReducers,
         ...cataloguesReducers.changeCatalogueNameReducers,
         ...cataloguesReducers.changeDefaultCatalogueReducers,
         ...cataloguesReducers.changePublicCatalogueReducers,
@@ -44,6 +49,7 @@ export const {
     CREATE_CATALOGUE, CREATE_CATALOGUE_START, CREATE_CATALOGUE_SUCCESS, CREATE_CATALOGUE_FAILURE, NEW_CATALOGUE_CREATED,
     REFRESH_CATALOGUE, REFRESH_CATALOGUE_START, REFRESH_CATALOGUE_SUCCESS, REFRESH_CATALOGUE_FAILURE,
     FETCH_CATALOGUES, FETCH_CATALOGUES_START, FETCH_CATALOGUES_SUCCESS, FETCH_CATALOGUES_FAILURE,
+    FETCH_AUTH_USER_CATALOGUES, FETCH_AUTH_USER_CATALOGUES_START, FETCH_AUTH_USER_CATALOGUES_SUCCESS, FETCH_AUTH_USER_CATALOGUES_FAILURE,
     TOGGLE_CATALOGUE_NAME_EDIT,
     CHANGE_CATALOGUE_NAME, FETCH_CATALOGUE_FIELDS, CHANGE_CATALOGUE_NAME_START, CHANGE_CATALOGUE_NAME_SUCCESS, CHANGE_CATALOGUE_NAME_FAILURE,
     CHANGE_DEFAULT_CATALOGUE, CHANGE_DEFAULT_CATALOGUE_START, CHANGE_DEFAULT_CATALOGUE_SUCCESS, CHANGE_DEFAULT_CATALOGUE_FAILURE,
