2dd437fa603a0a231f80194f61ad35a336bb1cf1
Adrian Bielawski <adrian@bielaw.ski>
2021-03-24 15:13:46 +0000

    Favourites added to nav in main header

--- src/components/global-components/header/header.tsx
+++ src/components/global-components/header/header.tsx
@@ -1,7 +1,13 @@
 import React, { useContext } from 'react'
+import { useHistory } from 'react-router'
 import icon from 'assets/img/icon.svg'
 import { faCog, faFolderOpen, faSignInAlt, faSignOutAlt } from '@fortawesome/free-solid-svg-icons'
+import { faHeart } from '@fortawesome/free-regular-svg-icons'
 import styles from './header.scss'
+//Types
+import { LocationState } from 'src/globalTypes'
+//Context
+import { NavContext } from '../nav/nav-store/navStore'
 //Redux
 import { LOG_OUT } from 'store/slices/authSlices/authSlices'
 import { useAppDispatch, useTypedSelector } from 'store/storeConfig'
@@ -9,14 +15,11 @@ import { useAppDispatch, useTypedSelector } from 'store/storeConfig'
 import Nav from '../nav/nav'
 import UserImage from '../user-image/userImage'
 import NavContextProvider from '../nav/nav-store/navContextProvider'
-import { NavContext } from '../nav/nav-store/navStore'
-import { useHistory } from 'react-router'
-import { LocationState } from 'src/globalTypes'
-import FavouriteIcon from '../favourite-icon/favouriteIcon'
 
 const contextValue = {
     show: false,
     listId: null,
+    nestedListId: null,
 }
 
 const Header = () => {
@@ -27,41 +30,28 @@ const Header = () => {
     const catalogues = useTypedSelector(state => state.catalogues)
 
     const handleLogout = () => {
-        dispatch(LOG_OUT({history}))
+        dispatch(LOG_OUT({ history }))
     }
 
     const NAV_ITEMS = user !== null ? [
-        // {
-        //     id: 'Favourite catalogues',
-        //     title: 'Favourite catalogues',
-        //     icon: (
-        //         <FavouriteIcon
-        //             className={styles.favouriteIcon}
-        //             active={props.catalogue.isFavourite}
-        //             onChange={handleFavouriteChange}
-        //         />
-        //     ),
-        //     children: [
-        //         {
-        //             id: 'My account',
-        //             title: 'My account',
-        //             faIcon: faCog,
-        //             url: `/${user!.username}/settings/account/my-account`,
-        //         },
-        //         {
-        //             id: 'Manage catalogues',
-        //             title: 'Manage catalogues',
-        //             faIcon: faCog,
-        //             url: `/${user!.username}/settings/account/manage-catalogues`,
-        //         },
-        //         {
-        //             id: 'Logout',
-        //             title: 'Logout',
-        //             faIcon: faSignOutAlt,
-        //             onClick: handleLogout,
-        //         }
-        //     ],
-        // },
+        {
+            id: 'Favourites',
+            title: 'Favourites',
+            faIcon: faHeart,
+            children: [
+                {
+                    id: 'Favourite catalogues',
+                    title: 'Favourite catalogues',
+                    faIcon: faFolderOpen,
+                    children: catalogues.authUser.favouriteCatalogues.map(c => ({
+                        id: c.name,
+                        title: c.name,
+                        faIcon: faFolderOpen,
+                        url: `/${user!.username}/catalogues/${c.slug}`,
+                    })),
+                }
+            ]
+        },
         {
             id: 'My catalogues',
             title: 'My catalogues',
