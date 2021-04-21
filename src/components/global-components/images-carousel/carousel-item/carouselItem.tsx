cc298bb4476d6a12742fff5f255f885469915025
Adrian Bielawski <adrian@bielaw.ski>
2021-04-21 14:52:56 +0100

    Show fully loaded images in ImagesCarousel

--- src/components/global-components/images-carousel/carousel-item/carouselItem.tsx
+++ src/components/global-components/images-carousel/carousel-item/carouselItem.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTrashAlt } from '@fortawesome/free-regular-svg-icons'
 import classNames from 'classnames/bind'
@@ -7,9 +7,12 @@ import styles from './carouselItem.scss'
 import { BASE_URL } from 'src/constants'
 //Types
 import { DeserializedImage } from 'src/globalTypes'
+//Hoks
+import { useImageLoader } from 'src/hooks/useImageLoader'
 //Custom components
 import TransparentButton from 'components/global-components/transparent-button/transparentButton'
 import PrimaryImageStar from '../primary-image-star/primaryImageStar'
+import Loader from 'components/global-components/loader/loader'
 
 type Props = {
     offset: string | null,
@@ -27,24 +30,32 @@ type Props = {
 const cx = classNames.bind(styles)
 
 const CarouselItem = (props: Props) => {
-    const onPrimaryChange = () => {
-        if (props.onPrimaryChange !== undefined) {
-            props.onPrimaryChange()
-        }
-    }
-
     const url = props.image.id.toString().startsWith('newImage')
         ? props.image.image
         : props.useThumbnails
             ? `${BASE_URL}${props.image.imageThumbnail}`
             : `${BASE_URL}${props.image.image}`
 
+    const image = useImageLoader(url)
+
+    const onPrimaryChange = () => {
+        if (props.onPrimaryChange !== undefined) {
+            props.onPrimaryChange()
+        }
+    }
 
     const carouselItemClass = cx(
         'carouselItem',
         {
             singleView: props.singleView,
             withShadow: props.withShadow,
+        }
+    )
+
+    const imageClass = cx(
+        'img',
+        {
+            loaded: image,
         }
     )
 
@@ -60,9 +71,12 @@ const CarouselItem = (props: Props) => {
                 '--scale': props.scale,
             } as React.CSSProperties}
         >
+            {!image && <Loader className={styles.loader} />}
             <img
-                src={url}
+                className={imageClass}
+                src={image || ''}
                 onClick={props.onImageClick}
+                key={props.image.id}
             />
             {props.onRemove &&
                 <TransparentButton
