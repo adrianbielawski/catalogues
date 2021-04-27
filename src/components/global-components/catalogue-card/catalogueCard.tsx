59f8ee82823033d2726e716429d254bbf4c346b9
Adrian Bielawski <adrian@bielaw.ski>
2021-04-27 07:53:01 +0100

    CatalogueCard styles refactored and redirect to catalogue added to image's onClick

--- src/components/global-components/catalogue-card/catalogueCard.tsx
+++ src/components/global-components/catalogue-card/catalogueCard.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useHistory } from 'react-router'
 import moment from 'moment'
+import { faUser } from '@fortawesome/free-solid-svg-icons'
 import classNames from 'classnames/bind'
 import styles from './catalogueCard.scss'
 //Types
@@ -9,7 +10,7 @@ import { DeserializedCatalogue, LocationState } from 'src/globalTypes'
 import { useTypedSelector } from 'store/storeConfig'
 import { userSelector } from 'store/selectors'
 //Custom components
-import Avatar from '../avatar/avatar'
+import AvatarWithName from '../avatar-with-name/avatarWithName'
 import NoImageIcon from '../no-image-icon/noImageIcon'
 import Image from 'components/global-components/image/image'
 
@@ -55,18 +56,17 @@ const CatalogueCard: React.ForwardRefRenderFunction<
                 placeHolder={
                     <NoImageIcon size="6x" />
                 }
+                onClick={redirectToCatalogue}
             />
             <div className={styles.meta}>
-                <div
+                <AvatarWithName
                     className={styles.user}
+                    name={user.username}
+                    placeholderIcon={faUser}
+                    url={user.imageThumbnail}
+                    avatarClassName={styles.userImage}
                     onClick={redirectToUser}
-                >
-                    <Avatar
-                        className={styles.avatar}
-                        url={user.imageThumbnail}
-                    />
-                    <p>{user.username}</p>
-                </div>
+                />
                 <p className={styles.date}>
                     {catalogue.itemsRanges.date.max &&
                         moment(catalogue.itemsRanges.date.max).fromNow()
